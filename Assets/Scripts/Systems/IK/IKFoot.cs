17dcdbe1181801b7179c7fb4375bc24299ccaa5e
Ben Willemsma <ben.willemsma@gmail.com>
2018-04-09 20:35:03 -0400

    Cyclops Foot IK, Random Grass texture for terrain

--- Assets/Scripts/Systems/IK/IKFoot.cs
+++ Assets/Scripts/Systems/IK/IKFoot.cs
@@ -6,30 +6,48 @@ public class IKFoot : MonoBehaviour
 {
     public bool rightfoot;
 
-    public CharacterData data;
-    public IKController IK;
+    public HumanoidData data;
+    private IKController IK;
+    private Animator anim;
 
     public Transform foot;
     public Transform heel;
     public Transform toes;
+    
+    private Vector3 lastPos;
 
     private Quaternion rotationOffset;
     private Vector3 positionOffset;
 
     private void Start()
     {
+        IK = data.IK;
+        anim = data.Anim;
+        if (!IK || !anim)
+        {
+            enabled = false;
+            return;
+        }
+
         transform.parent = null;
         positionOffset = transform.position - foot.position;
+        lastPos = transform.position;
 
         if (rightfoot)
         {
             IK.RightFoot.rotationWeight = 1;
             IK.RightFoot.rotation = foot.rotation;
+
+            IK.RightFoot.positionWeight = 1;
+            IK.RightFoot.position = foot.position;
         }
         else
         {
             IK.LeftFoot.rotationWeight = 1;
             IK.LeftFoot.rotation = foot.rotation;
+
+            IK.LeftFoot.positionWeight = 1;
+            IK.LeftFoot.position = foot.position;
         }
     }
 
@@ -42,23 +60,37 @@ public class IKFoot : MonoBehaviour
 
     private void AdjustFootIK()
     {
-        Vector3 heelPoint = heel.position;
+        Vector3 heelPoint = Vector3.zero;
         RaycastHit heelHit;
-        if (Physics.Raycast(heel.position + Vector3.up * 2, Vector3.down, out heelHit, 10))
+        if (Physics.Raycast(heel.position + Vector3.up * 5, Vector3.down, out heelHit, 10))
             heelPoint = heelHit.point;
 
-        Vector3 toePoint = toes.position;
+        Vector3 toePoint = Vector3.zero;
         RaycastHit toeHit;
-        if (Physics.Raycast(toes.position + Vector3.up * 2, Vector3.down, out toeHit, 10))
+        if (Physics.Raycast(toes.position + Vector3.up * 5, Vector3.down, out toeHit, 10))
             toePoint = toeHit.point;
 
-        Vector3 forward = Vector3.ProjectOnPlane(toePoint - heelPoint, data.transform.right);
-        Debug.DrawRay(transform.position, toePoint - heelPoint, Color.magenta);
-        Quaternion newRotation = Quaternion.LookRotation(forward, Vector3.up);
+        if (heelPoint != Vector3.zero && toePoint != Vector3.zero)
+        {
+            // Rotation
+            Vector3 forward = Vector3.ProjectOnPlane(toePoint - heelPoint, data.transform.right);
+            Quaternion newRotation = Quaternion.LookRotation(forward, Vector3.up);
 
-        if (rightfoot) IK.RightFoot.rotation = Quaternion.Lerp(IK.RightFoot.rotation, newRotation, Time.deltaTime * 5); 
-        else IK.LeftFoot.rotation = Quaternion.Lerp(IK.RightFoot.rotation, newRotation, Time.deltaTime * 5); 
+            if (rightfoot) data.IK.RightFoot.rotation = Quaternion.Lerp(IK.RightFoot.rotation, newRotation, Time.deltaTime * 5);
+            else IK.LeftFoot.rotation = Quaternion.Lerp(IK.LeftFoot.rotation, newRotation, Time.deltaTime * 5);
 
-        Debug.DrawRay(transform.position, foot.forward, Color.red);
+            // Position
+            Vector3 newPosition = heelPoint - heel.localPosition - positionOffset;
+            if (rightfoot)
+            {
+                IK.RightFoot.positionWeight = anim.GetFloat("RightFoot");
+                IK.RightFoot.position = Vector3.Lerp(IK.RightFoot.position, newPosition, Time.deltaTime * 5);
+            }
+            else
+            {
+                IK.LeftFoot.positionWeight = anim.GetFloat("LeftFoot");
+                IK.LeftFoot.position = Vector3.Lerp(IK.LeftFoot.position, newPosition, Time.deltaTime * 5);
+            }
+        }
     }
 }
